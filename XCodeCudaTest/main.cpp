6c717e50cd932d745ac108c08c1bac454efd51fd
takmatsumoto <motsidakaori@gmail.com>
2011-11-08 16:42:14 +0800

    Initial Commit

---
+++ XCodeCudaTest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  XCodeCudaTest
+//
+//  Created by  on 2011/11/8.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
