caebe07622010f26e0102ffa19575f6ed6db5868
takmatsumoto <motsidakaori@gmail.com>
2011-11-09 17:02:08 +0800

    取得CUDA裝置，取得CUDA內容..CUcontext

--- XCodeCudaTest/main.cpp
+++ XCodeCudaTest/main.cpp
@@ -28,13 +28,45 @@ int main (int argc, const char * argv[])
 bool InitCuda() {
     
     int deviceCount;
+    CUdevice device;
     
-    cuInit(0);
+    // 初始化CUDA裝置
+    if (cuInit(0) != CUDA_SUCCESS)
+        return false;
+    
+    // 取得裝置數量
     cuDeviceGetCount(&deviceCount);
     
+    // 取得裝置
+    if (deviceCount>0) {
 #ifdef DEBUG
-    printf("CUDA Device Count : %d\n",deviceCount);
+        printf("CUDA Device Count : %d\n",deviceCount);
 #endif    
+        for (int i=0; i<deviceCount; i++) {
+            
+            if (cuDeviceGet(&device, i)==CUDA_SUCCESS) {
+                break;
+            }
+            else {
+                printf("get device failure\n");
+                return false;
+            }
+        }
+    }
+    else {
+        printf("No CUDA device in this computer\n");
+        return false;
+    }
+    
+    // 取得裝置內容 (create context)
+    CUcontext context;
+    cuCtxCreate_v2(&context, 0, device);
+    
+    char deviceName[1024] = {0};
+    cuDeviceGetName(deviceName, 1024, device);
+#ifdef DEBUG
+    printf("Device Name : %s\n",deviceName);
+#endif
     
     
     return true;
