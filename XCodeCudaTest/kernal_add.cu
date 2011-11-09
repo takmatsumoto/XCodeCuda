bf7e46d374e24438176fa3261c5298ff9487ad38
takmatsumoto <motsidakaori@gmail.com>
2011-11-09 16:58:34 +0800

    新增cu檔

--- XCodeCudaTest/kernal_add.cu
+++ XCodeCudaTest/kernal_add.cu
@@ -1,9 +1,13 @@
 //
-//  kernal_add.c
+//  kernal_add.cu
 //  XCodeCudaTest
 //
 //  Created by  on 2011/11/9.
-//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2011年 takmatsumoto All rights reserved.
 //
 
-#include <stdio.h>
+
+//__global__ void VecAdd(float* A, float* float* B, float* C) 
+//{
+//    int idx = threadIdx.x;
+//}
